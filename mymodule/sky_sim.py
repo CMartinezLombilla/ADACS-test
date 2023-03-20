b7134891e8f907b462b54e3205353d4e24d564b2
CMartinezLombilla <crismarlom@gmail.com>
2023-03-20 14:36:31 +1100

    Trailing whitespaces fixed

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -11,10 +11,10 @@ A script to simulate a catalogue of simulated stars around Andromeda galaxy
 import math
 import random
 
-# Determine Andromeda location in ra/dec degrees 
+# Determine Andromeda location in ra/dec degrees
 # from wikipedia
-RA = '00:42:44.3' 
-DEC = '41:16:09'  
+RA = '00:42:44.3'
+DEC = '41:16:09'
 
 # convert to decimal degrees
 d, m, s = DEC.split(':')
@@ -35,7 +35,7 @@ for i in range(NSRC):
 
 
 # now write these to a csv file for use by my other program
-with open('catalog.csv','w', encoding='utf-8') as f:  
+with open('catalog.csv','w', encoding='utf-8') as f:
     print("id,ra,dec", file=f)
     for i in range(NSRC):
         print(f"{0:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
