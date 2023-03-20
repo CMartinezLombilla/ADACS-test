f56ba45d860066fd4b4f76ce397de5066d9da140
CMartinezLombilla <crismarlom@gmail.com>
2023-03-20 14:34:35 +1100

    math and random modules at top and imported

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -8,34 +8,30 @@ Created on Mon Mar 20 10:49:18 2023
 A script to simulate a catalogue of simulated stars around Andromeda galaxy 
 """
 
+import math
+import random
+
 # Determine Andromeda location in ra/dec degrees 
 # from wikipedia
 RA = '00:42:44.3' 
 DEC = '41:16:09'  
 
 # convert to decimal degrees
-from math import cos, pi  
-
 d, m, s = DEC.split(':')
 DEC = int(d)+int(m)/60+float(s)/3600
 
 h, m, s = RA.split(':')
 RA = 15*(int(h)+int(m)/60+float(s)/3600)
-RA = RA/cos(DEC*pi/180)
+RA = RA/math.cos(DEC*math.pi/180)
 
 NSRC = 1_000_000
 
 # make 1000 stars within 1 degree of Andromeda
-from random import *
-# ^- Wildcard import math (wildcard-import)
-# ^- Import "from math import *" should be placed at the top of the module (wrong-import-position)
-# ^- Unused import(s) NV_MAGICCONST, TWOPI, LOG4, SG_MAGICCONST, BPF, RECIP_BPF, Random, SystemRandom, seed, random, triangular, randint, choice, randrange, sample, shuffle, choices, normalvariate, lognormvariate, expovariate, vonmisesvariate, gammavariate, gauss, betavariate, paretovariate, weibullvariate, getstate, setstate and getrandbits from wildcard import of random (unused-wildcard-import)
-
 ras = []
 decs = []
 for i in range(NSRC):
-    ras.append(RA + uniform(-1,1))
-    decs.append(DEC + uniform(-1,1))
+    ras.append(RA + random.uniform(-1,1))
+    decs.append(DEC + random.uniform(-1,1))
 
 
 # now write these to a csv file for use by my other program
