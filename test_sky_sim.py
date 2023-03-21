aeb6d289d74b3d487a5c36eeaa02f669b27279d5
CMartinezLombilla <crismarlom@gmail.com>
2023-03-21 12:36:54 +1100

    pytest numpy.testing

---
+++ test_sky_sim.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 12:00:02 2023
+
+@author: C. Martinez-Lombilla
+"""
+
+
+'''
+
+La manera de hacer funciones para testear codigos:
+    
+    
+def test_that_a_thing_works():
+    # do things
+    if not thing_works:
+        raise AssertionError("Description of what failed")
+    return
+'''
+
+from numpy import testing
+
+
+def test_module_import():
+    try:
+        import mymodule.sky_sim
+    except Exception as e:
+        raise AssertionError("Failed to import mymodule")
+    return
+
+
+
+def test_get_radec():
+    from mymodule import sky_sim
+    
+    answer = (14.215420962967535, 41.26916666666667)
+    result = sky_sim.get_radec()
+    testing.assert_allclose(answer, result, rtol=0.1)
+    
+    if answer!= result:
+        raise AssertionError(f'Position of Andromeda is wrong.\nShould be {answer}, but go {result}')
+    return
